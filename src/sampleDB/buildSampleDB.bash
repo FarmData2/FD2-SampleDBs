7662060e7520b3653ff07fa4bc7ce949ff698d26
Grant Braught <braught@dickinson.edu>
2024-06-24 15:38:33 -0400

    adds direct seedings to sample database

--- src/sampleDB/buildSampleDB.bash
+++ src/sampleDB/buildSampleDB.bash
@@ -73,6 +73,11 @@ node "$SCRIPT_DIR/addTraySeedings.js"
 error_check
 echo ""
 
+# Add direct seedings
+node "$SCRIPT_DIR/addDirectSeedings.js"
+error_check
+echo ""
+
 # Delete the authentication token if it exists.
 # Necessary because base DB was reinstalled so old token is not valid. 
 echo "Deleting locally cached authentication token..."
